e202a11f112940a149f66585415b34f908392f79
Mykola Popeliuk <mycolapopelyuk@gmail.com>
2021-10-29 17:02:19 +0300

    modified asynchron part and deleted unused try catch

--- src/ReactStreams.scala
+++ src/ReactStreams.scala
@@ -1,8 +1,8 @@
 import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 
+import java.lang.Thread.interrupted
 import scala.collection.mutable
 import scala.util.Random
-import scala.util.control.Breaks.break
 
 object Main1 extends App {
   var cache: mutable.Map[String, Byte] = new mutable.HashMap[String, Byte];
@@ -24,30 +24,19 @@ class CacheEditor1(var cache: mutable.Map[String, Byte]) extends Actor {
 
   override def receive = {
     case key: String => {
-      try{
-        val oldValue: Byte = (cache.get(key)).getOrElse(0)
-        while (true) {
 
-          val newValue: Byte = (oldValue + 1).toByte
+      var oldValue: Byte = (cache.get(key)).getOrElse(0)
+      var newValue: Byte = (oldValue + 1).toByte
+      do {
+        oldValue = (cache.get(key)).getOrElse(0)
+        newValue = (oldValue + 1).toByte
 
+      } while (oldValue != cache.get(key).getOrElse(0))
 
-          if (oldValue == cache.get(key).getOrElse(0)) {
-            cache.put(key, newValue)
-            println(s"putting in $key value $newValue")
-            break;
-          }
-
-        }
-      }
-      catch{
-        case _ => println("Something exactly wrong")
-
-      }
-
-
-
+      cache.put(key, newValue)
+      println(s"putting in $key value $newValue")
     }
-    case _ => println("something wrong")
+    case _ => println("message can not be received")
   }
 }
 
@@ -58,10 +47,11 @@ class MassageEmulator(sendTo: ActorRef, val values: List[String]) extends Runnab
     val random: Random = new Random();
     val actorName: String = sendTo.path.name
 
-    while (true) {
+    while (!interrupted()) {
       sendTo ! values(random.nextInt(values.length))
 
       println(s"sending changing to $actorName")
+
       Thread.sleep(random.nextInt(1000) + 500)
 
     }
